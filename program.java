2501b7d6e8a3c10f3d5700c001b84244f591370f
zekiamani1 <zekiamanioke@gmail.com>
2026-02-18 02:04:53 +0700

    feat: added brute forcing it

---
+++ program.java
@@ -0,0 +1,134 @@
+import java.util.Scanner;
+
+public class program{
+    public static long iterasi = 0;
+    public static boolean cek(int x,int y, char map[][][]){
+        //cek sekolom
+        for (int i = 0; i < map.length; i++) {
+            if(map[i][y][1]=='Y' && i!=x){
+                return false;
+            }
+        }
+        //cek serow 
+        for (int i = 0; i < map.length; i++) {
+            if(map[x][i][1]=='Y' && i!=y){
+                return false;
+            }
+        }
+        //cek adjacent diagonal
+        if(x+1 < map.length && y+1 < map.length && map[x+1][y+1][1]=='Y') 
+            {return false;}
+        if(x+1 < map.length && y-1 >= 0 && map[x+1][y-1][1]=='Y') 
+            {return false;}
+        if(x-1 >= 0 && y+1 < map.length && map[x-1][y+1][1]=='Y') 
+            {return false;}
+        if(x-1 >= 0 && y-1 >= 0 && map[x-1][y-1][1]=='Y') 
+            {return false;}
+        
+        //cek warna
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map.length; j++) {
+                if(map[i][j][1]=='Y' && map[i][j][0]==map[x][y][0] && i!=x && j!=y){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static boolean isvalid(char[][][] map) {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map.length; j++) {
+                if(map[i][j][1]!='Y') continue;
+                if(!cek(i,j,map)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static char[][][] bruteforce(char[][][] map){
+        int n=map.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    for (int l = 0; l < n; l++) {
+                        for (int m = 0; m < n; m++) {
+                            for (int o = 0; o < n; o++) {
+                                for (int p = 0; p < n; p++) {
+                                    for (int q = 0; q < n; q++) {
+                                        for (int r = 0; r < n; r++) {
+                                            iterasi++;
+                                            map[0][i%n][1] = 'Y';
+                                            map[1][j%n][1] = 'Y';
+                                            map[2][k%n][1] = 'Y';
+                                            map[3][l%n][1] = 'Y';
+                                            map[4][m%n][1] = 'Y';
+                                            map[5][o%n][1] = 'Y';
+                                            map[6][p%n][1] = 'Y';
+                                            map[7][q%n][1] = 'Y';
+                                            map[8][r%n][1] = 'Y';
+                                            if(isvalid(map)){
+                                                return map;
+                                            } else {
+                                                map[0][i%n][1] = 'N';
+                                                map[1][j%n][1] = 'N';
+                                                map[2][k%n][1] = 'N';
+                                                map[3][l%n][1] = 'N';
+                                                map[4][m%n][1] = 'N';
+                                                map[5][o%n][1] = 'N';
+                                                map[6][p%n][1] = 'N';
+                                                map[7][q%n][1] = 'N';
+                                                map[8][r%n][1] = 'N';
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        char map[][][]=new char[n][n][2];
+
+        in.nextLine();
+        for (int i = 0; i < n; i++) {
+            String temp=in.nextLine();
+            for (int j = 0; j < n; j++) {
+                map[i][j][1]='N';
+                map[i][j][0]=temp.charAt(j);
+            }
+        }
+
+        char[][][] map2;
+        
+        long start = System.nanoTime();
+        map2=bruteforce(map);
+        long end = System.nanoTime();
+        
+        double interval = (end - start) / 1_000_000_000.0;
+        
+        System.out.println("jumlah iterasi: " + iterasi);
+        System.out.printf("waktu pencarian: %.6f detik%n", interval);
+        System.out.println();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(map2[i][j][1]=='Y'){
+                    System.out.print('#');
+                } else{
+                    System.out.print(map2[i][j][0]);
+                }
+            }
+            System.out.println();
+        }
+
+        in.close();
+    }
+}
